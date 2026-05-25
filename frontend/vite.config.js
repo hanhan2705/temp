1cabe8396f7244563e583012a6b6f438be821c5b
hanhan2705 <luongvohanhan@gmailcom>
2026-05-25 16:10:53 +0700

    fix vite allowed hosts

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: 'all',
-    host: true,
-    port: 5173
+    host: '0.0.0.0',
+    port: 5173,
+    allowedHosts: [
+      'soothing-kindness-production-f3dd.up.railway.app',
+      'all'
+    ]
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 5173,
+    allowedHosts: [
+      'soothing-kindness-production-f3dd.up.railway.app',
+      'all'
+    ]
   }
 })
